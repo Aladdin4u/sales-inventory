a5fc3124232d05aab72bfed759b8a1b70b43be49
Aladdin4u <taofikakinkumi@gmail.com>
2023-07-24 03:56:55 +0100

    new features

--- customer/customer-service.test.js
+++ customer/customer-service.test.js
@@ -1,6 +1,6 @@
 // which service it is
 describe("customerService", () => {
-    // which function
+    // which function we are testing
     describe("siginIn", () => {
         // which scenrio we are testing
         test("validate user input", () => {
